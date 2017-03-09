7414145bb3bb60e66883fd9bd60437da122158db
will555 <waitk1@ocdsb.ca>
2017-03-09 14:01:37 -0500

    Add files via upload

---
+++ p5/House/sketch.js
@@ -0,0 +1,14 @@
+
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+rect(1, 338, 398, 60);
+ellipse(200, 300, 150, 150);
+ellipse(200, 200, 100, 100);
+ellipse(200, 120, 75, 75);
+line(160, 200, 6, 77);
+line(240, 200, 267, 10);
+}
