a0648bc8fd809e59364209875786caabe2ee13a8
will555 <waitk1@ocdsb.ca>
2017-03-10 12:23:45 -0500

    Update sketch.js

--- p5/House/sketch.js
+++ p5/House/sketch.js
@@ -8,15 +8,17 @@ function draw() {
 var roof= 41;
 
 var wall=133;
+  
+var houseWidth=100;
 
-fill(156, 201, 255);
-rect(139,209,wall,wall);
+fill(houseWidth+56,houseWidth*2, houseWidth+155);
+rect(139,houseWidth*2+9,wall,wall);
 fill(0, 0, 0);
-rect(196,297,23,43);
+rect(houseWidth+96,houseWidth*3,23,43);
 fill(255, 255, 255);
-rect(201,302,13,13);
-rect(160,240,30,30);
-rect(219,240,30,30);
+rect(houseWidth*2,houseWidth*3,13,13);
+rect(houseWidth+60,houseWidth*2+40,30,30);
+rect(houseWidth*2+19,houseWidth*2+40,30,30);
 fill(130, 0, 0);
 triangle(300,212,205,124,110,211);
 fill(255, 255, 255);
