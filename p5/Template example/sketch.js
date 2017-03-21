1b263f248263d8990e022ce646e02d08d809504d
will555 <waitk1@ocdsb.ca>
2017-03-21 14:13:08 -0400

    Update sketch.js

--- p5/Template example/sketch.js
+++ p5/Template example/sketch.js
@@ -3,5 +3,20 @@ function setup() {
 }
 
 function draw() {
-  ellipse(200, 200, 80, 80);
+// The ground
+background(84, 229, 255);
+fill(99, 255, 104);
+rect(0, 300, 400, 100);  
+
+// The sun
+fill(247, 255, 10);
+ellipse(80, 64, 100, 100);  
+
+// The snowman
+fill(255, 255, 255);
+ellipse(200, 300, 150, 150);
+ellipse(200, 200, 100, 100);
+ellipse(200, 120, 75, 75);
+line(151,193,65,122);
+line(249,204,332,283);
 }
