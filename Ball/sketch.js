e5e1478e9599d4be7bc8a3184eb7a0ad0b6e60d1
will555 <waitk1@ocdsb.ca>
2017-04-13 13:05:09 -0400

    Update sketch.js

--- Ball/sketch.js
+++ Ball/sketch.js
@@ -2,6 +2,34 @@ function setup() {
   createCanvas(640, 480);
 }
 
-function draw() {
-  ellipse(200, 200, 80, 80);
-}
+// position of the ball
+var y = 0;
+var x = 0;
+// how far the ball moves every time
+var speed = 2;
+
+draw = function() {
+    background(127, 204, 255);
+    
+    fill(66, 66, 66);
+    ellipse(x, y, 50, 50);
+
+    // move the ball
+    y = y + speed;
+    x = x + speed;
+    
+    if (y > 400) {
+        speed =-2;
+    }
+    if (y < 0) {
+        speed = 3;
+    }
+    if (x < 0) {
+        speed = 3;
+    }
+    if (x > 400) {
+        speed = -2;
+    }
+};
+
+
