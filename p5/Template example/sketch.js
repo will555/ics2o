61400edb148ed6a2045366504992bbae513256c8
will555 <waitk1@ocdsb.ca>
2017-04-13 13:00:17 -0400

    Add files via upload

--- p5/Template example/sketch.js
+++ p5/Template example/sketch.js
@@ -3,21 +3,5 @@ function setup() {
 }
 
 function draw() {
-// The ground
-background(84, 229, 255);
-fill(99, 255, 104);
-rect(0, 300, 400, 100);  
-
-// The sun
-fill(247, 255, 10);
-ellipse(80, 64, 100, 100);  
-
-// The snowman
-fill(255, 255, 255);
-ellipse(200, 300, 150, 150);
-ellipse(200, 200, 100, 100);
-ellipse(200, 120, 75, 75);
-line(151,193,65,122);
-line(249,204,332,283);
+  ellipse(200, 200, 80, 80);
 }
-https://will555.github.io/ics2o/p5/Template%20example/index.html
