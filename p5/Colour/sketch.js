4ddb755043498cc4c09dd2d6e80b3e343c2288ec
will555 <waitk1@ocdsb.ca>
2017-04-25 14:05:07 -0400

    Update sketch.js

--- p5/Colour/sketch.js
+++ p5/Colour/sketch.js
@@ -12,16 +12,16 @@ var colour3 = 0;
 
 var draw=function () {
 background(194, 194, 194);
-if (keyIsDown(87)) {
+if (keyIsDown(38)) {
       colour = colour + 5;
     }
-if (keyIsDown(65)) {
+if (keyIsDown(37)) {
       colour2 = colour2 + 5;
     }
-if (keyIsDown(68)) {
+if (keyIsDown(39)) {
       colour3 = colour3 + 5;
     }
-if (keyIsDown(83)) {
+if (keyIsDown(40)) {
       colour = 0;
       colour2 = 0;
       colour3 = 0;
