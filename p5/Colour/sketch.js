5bad1128e2976124a0ae2839724b303ceceeb596
will555 <waitk1@ocdsb.ca>
2017-04-25 13:15:27 -0400

    Update sketch.js

--- p5/Colour/sketch.js
+++ p5/Colour/sketch.js
@@ -1,7 +1,38 @@
-function setup() {
+/*Will
+Colour
+25-4-2017*/
+var y = 0;
+var x = 0;
+var bugSize = 20;
+var colour = 0;
+var colour2 = 0;
+var colour3 = 0;
+
+  function setup() {
   createCanvas(640, 480);
 }
 
-function draw() {
-  ellipse(200, 200, 80, 80);
-}
+
+
+var draw=function () {
+background(194, 194, 194);
+if (keyIsPressed && keyCode === 38) {
+      colour = colour + 5;
+    }
+if (keyIsPressed && keyCode === 37) {
+      colour2 = colour2 + 5;
+    }
+if (keyIsPressed && keyCode === 39) {
+      colour3 = colour3 + 5;
+    }
+if (keyIsPressed && keyCode === 40) {
+      colour = 0;
+      colour2 = 0;
+      colour3 = 0;
+    }
+
+fill(colour, colour2, colour3);
+  rect(100,100,200,200,29);
+  textSize(23);
+  text("Make Your Favorite Colour",62,66);
+};
