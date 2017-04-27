29f516cfa3e175c3e09407bf3ed352bd12215e95
will555 <waitk1@ocdsb.ca>
2017-04-27 14:04:25 -0400

    Update sketch.js

--- p5/BUG/sketch.js
+++ p5/BUG/sketch.js
@@ -15,48 +15,60 @@ function setup() {
 function bug () {
 //Bug
   fill(36, 116, 255);
+  //left top leg
   rect(x+bugSize*-1.36,y+bugSize*1.41,32,11);
+  //right top leg
   rect(x+bugSize*1.86,y+bugSize*1.41,32,11);
+  //left bottom leg
   rect(x+bugSize*-1.36,y+bugSize*2.56,38,11);
+  //right bottom leg
   rect(x+bugSize*1.89,y+bugSize*2.45,32,11);
   fill(38, 0, 255);
+  //body of the bug
   ellipse(x+bugSize*1.14,y+bugSize*2.05,54,57);
+  //head of the bug
   ellipse(x+bugSize*1.14,y+bugSize*0.45,40,40);
 }
 
 function draw() {
     background(127, 204, 255);
     bug();
-   //move Bug
-
+   
+   //moves the Bug
     y = y + speed;
     x = x + speed;
-    
+   //stops the bug from moving off the screen
     if (y > 400) {
         speed =-2;
        } 
+   //changes the speed of the bug if the mouse is pressed
       else if (mouseIsPressed) {
             speed =-4;
         }
-    
+   //stops the bug from moving off the screen
     if (y < 0) {
         speed = 2;
     }
+  //stops the bug from moving off the screen
     if (x < 0) {
         speed = 2;
     }
+  //stops the bug from moving off the screen
     if (x > 400) {
         speed = -2;
     }
+  //shows the sprey bottle when the mouse id pressed
     if (mouseIsPressed) {
       spray();
     }
   
     fill(196, 164, 164);
+  //the food
     rect(334,338,54,54,23);
     fill(0, 0, 0);
     text('food',350,371);
     fill(250, 115, 115);
+  //the bugs house
     rect(-20,-12,115,95,53);
     fill(0, 0, 0);
     text('BUG HOUSE',7,42);
